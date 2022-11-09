5073c823476487703025576af868e941ba671248
Ultimate <thelamentablegamer69@outlook.com>
2022-11-08 19:07:27 -0500

    Added auto welcome msg

--- index.js
+++ index.js
@@ -34,24 +34,14 @@ client.loadButtons(bot, false)
 
 module.exports = bot
 
-// client.on("ready", () => {
-//   console.log(`Logged in as ${client.user.tag}`)
-// })
+const welcomeChannelId = "894965798723002408"
 
-// client.on("messageCreate", (message) => {
-//   if (message.content == "hi"){
-//     message.reply("Hello World!")
-//   }
-// })
-
-// const welcomeChannelId = "762780000936001626"
-
-// client.on("guildMemberAdd", async (member) => {
-//   const img = await generateImage(member)
-//   member.guild.channels.cache.get(welcomeChannelId).send({
-//     content: `<@${member.id}> Welcome to the server!`,
-//     files: [img]
-//   })
-// })
+client.on("guildMemberAdd", async (member) => {
+  const img = await generateImage(member)
+  member.guild.channels.cache.get(welcomeChannelId).send({
+    content: `<@${member.id}> Welcome to the server!`,
+    files: [img]
+  })
+})
 
 client.login(process.env.TOKEN)
